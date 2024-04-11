f4b7c2b2e886e8b475095d885b5131fed4a9347f
Noahduv <duvarneynoah@gmail.com>
2024-04-11 16:14:17 -0400

    #3
    added Modde: "cors"

--- public/main.js
+++ public/main.js
@@ -5,7 +5,7 @@
 
 const getCustomer = async (email) => {
     try{
-        const config = { headers: { Accept: 'application/json' }}
+        const config = { mode: "cors", headers: { Accept: 'application/json',  host: 'https://magnificent-gold-hatchling.cyclic.app/',  } }
         const res = await axios.get('https://mistymountain.cetecerp.com/api/customer?preshared_token=c4tBewPhEYNM1Gm&acct_email=${email}', config);
       //  const data = await res.json();
         console.log(res.data);
